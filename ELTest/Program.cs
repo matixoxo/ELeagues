378ae086fa73c373751f6b82cc47915d3935a776
krimaus <krimaus01@gmail.com>
2023-03-16 20:56:01 +0100

    added comment

--- ELTest/Program.cs
+++ ELTest/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Dziala");
             Console.Writeline("pull");
+            //to jest komentarz
         }
     }
 }
